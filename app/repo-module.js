986e8c2648d7f23f087f0c27f5d77626248b3efb
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-17 22:13:51 -0700

    added angular stuff and css angular: added followers and following stars but need to display counts css: background,icomoon and avatar resized

--- app/repo-module.js
+++ app/repo-module.js
@@ -13,22 +13,40 @@
     this.myAcct = {};
     this.repoUrl;
     this.repoLinks;
-    this.newDataSet;
+    this.newDataSet;//for getRepoLinks
+    this.copiedNewDataSet;//for getFollowers
+    this.followers = null;
+    this.following = null;
+    
+    //initial page load
+    $http.get(mainRoute).then((user)=>{
+      this.repoStuff = angular.toJson(user.data);
+      this.avatar = user.data.avatar_url;
+      this.myAcct = user.data.url;
+      this.repoUrl = user.data.repos_url
+      this.followers = user.data.followers
+      this.following = user.data.following
 
-  
-    this.getRepo = function(){
-      $http.get(mainRoute).then((user)=>{
-        this.repoStuff = angular.toJson(user.data);
-        this.avatar = user.data.avatar_url;
-        this.myAcct = user.data.url;
-        this.repoUrl = user.data.repos_url
-        console.dir('Here is ' + user.starred_url);
-        console.dir('Here is avatar' + angular.toJson(this.avatar));
-        console.dir('Here is myAcct' + angular.toJson(this.myAcct));
-        console.dir('repo : ' + this.repoUrl)
-      });
+      console.dir('Here is ' + user.starred_url);
+      console.dir('Here is avatar' + angular.toJson(this.avatar));
+      console.dir('Here is myAcct' + angular.toJson(this.myAcct));
+      console.dir('repo : ' + this.repoUrl)
+      console.dir('following : ' + this.following)
+      console.dir('followers : ' + this.followers)
 
-    };
+    });
+
+    this.getFollowers = function(){
+      if(this.copiedNewDataSet == null){
+        console.log('it is indeed 0');
+        this.getRepoLinks();
+      } else {
+        console.dir('it has some data! ' + angular.toJson(this.copiedNewDataSet))
+
+      }
+    }
+
+    //getting list of repositories in list view
     this.getRepoLinks = function(){
       $http.get(this.repoUrl).then((repo)=>{
         var dataSet = repo.data;
@@ -36,15 +54,20 @@
           var returnedObj = {
             name: '',
             html_url: '',
-            created_at: ''
+            created_at: '',
+            following_url: '',
+            followers_url: '',
+            starred_url: ''
           };
           returnedObj.name = obj.name
           returnedObj.html_url = obj.html_url
           returnedObj.created_at = obj.created_at
+          returnedObj.following_url = obj.owner.following_url
+          returnedObj.followers_url = obj.owner.followers_url
+          returnedObj.starred_url = obj.owner.starred_url
           return returnedObj
         })
-
-
+        this.copiedNewDataSet = angular.copy(this.newDataSet)
         console.dir('newDataSet  : ' + angular.toJson(this.newDataSet))
       })
     };
